e9beefc97359cccf1c1b989758a7b240c5551bb4
Jonathan Lee <jlee4889@gmail.com>
2025-06-09 23:12:43 +0100

    remake entire project using bootstrap
    made responsive design more effective

--- script.js
+++ script.js
@@ -1,190 +1,246 @@
-var combos={
-	"also": ["horse","person","woman"],
-	"day": ["day","door","moon"],
-	"door": ["day","horse","moon","mouth","one","person","wood"],
-	"horse": ["also","door","mouth","woman"],
-	"moon": ["day","door","moon"],
-	"mouth": ["door","horse","ten","woman","wood"],
-	"one": ["door","one","ten","two","wood"],
-	"person": ["also","door","son","ten","two","wood"],
-	"son": ["person","woman","wood"],
-	"ten": ["mouth","one","person"],
-	"two": ["one","person"],
-	"woman": ["also","horse","mouth","son"],
-	"wood": ["door","mouth","one","person","son","wood"]
+const combos = {
+	"also": ["horse", "person", "woman"],
+	"day": ["day", "door", "moon"],
+	"door": ["day", "horse", "moon", "mouth", "one", "person", "wood"],
+	"horse": ["also", "door", "mouth", "woman"],
+	"moon": ["day", "door", "moon"],
+	"mouth": ["door", "horse", "ten", "woman", "wood"],
+	"one": ["door", "one", "ten", "two", "wood"],
+	"person": ["also", "door", "son", "ten", "two", "wood"],
+	"son": ["person", "woman", "wood"],
+	"ten": ["mouth", "one", "person"],
+	"two": ["one", "person"],
+	"woman": ["also", "horse", "mouth", "son"],
+	"wood": ["door", "mouth", "one", "person", "son", "wood"]
 };
-
-var clears={
+const clears = {
 	1: 5,
 	2: 15,
 	3: 30,
 	4: 50
-}
+};
+const cellListeners = new Map();
+const images = ["also", "day", "door", "horse", "moon", "mouth",
+    "one", "person", "son", "ten", "two", "woman", "wood"];
+var words = [];
 
-var combo = [];
-var timeout;
-
-function changeImage(image){
-	images = ["also","day","door","horse","moon","mouth","one","person","son","ten","two","woman","wood"];
-	let newImage = images[getRndInteger(0,images.length-1)];
-	document.getElementById(image).src=`./images/${newImage}.png`
-	console.log(newImage);
-}
-
-function changeImage2(image){
-	images = ["also","day","door","horse","moon","mouth","one","person","son","ten","two","woman","wood"];
-	let newImage = images[getRndInteger(0,images.length-1)];
-	document.querySelector(image).src=`./images/${newImage}.png`;
-	console.log(newImage);
-}
-
-function sound(src) {
-  this.sound = document.createElement("audio");
-  this.sound.src = src;
-  this.sound.setAttribute("preload", "auto");
-  this.sound.setAttribute("controls", "none");
-  this.sound.style.display = "none";
-  document.body.appendChild(this.sound);
-  this.play = function(){
-    this.sound.play();
-  }
-  this.stop = function(){
-    this.sound.pause();
-  }
-}
-
+// Sound functions
 var ding = new sound("audio/ding.ogg");
 var levelUpSound = new sound("audio/level up.mp3");
-/*var bgMusic = new sound("audio/4-01-Mods_de_Chocobo.mp3");*/
 
-function increaseScore(){
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }
+}
+
+document.getElementById("bgm").addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    },
+false);
+document.getElementById("bgm").loop = true;
+
+// Variable-related functions
+function increaseScore() {
 	ding.play();
-	var score = Number(document.getElementById("score").innerHTML.slice(11));
-	document.getElementById("score").innerHTML = "分數 (Score): " + String(score+1);
-	return score+1;
+    score ++;
+	updateScoreDisplay();
 }
 
-function levelUp(){
+function updateScoreDisplay() {
+    document.getElementById("score").innerHTML = "分數 (Score): " + String(score);
+}
+
+function levelUp() {
 	levelUpSound.play();
-	var level = Number(document.getElementById("level").innerHTML.slice(12));
-	document.getElementById("level").innerHTML = "等級 (Level): " + String(level+1);
-	alert(`You are now at level ${level+1}!`);
+    level ++;
+    updateLevelDisplay();
+	alert(`You are now at level ${level}!`);
 }
 
-function placeCursor(image) {
-	var d = document.getElementById("cursor");
-	let elem = document.querySelector(image);
-	let rect = elem.getBoundingClientRect();
-	d.style.left = rect["x"]+'px';
-	d.style.top = rect["y"]+'px';
-	d.style.opacity = 1;
-	console.log(rect);
+function updateLevelDisplay() {
+    document.getElementById("level").innerHTML = "等級 (Level): " + String(level);
 }
 
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+// Handling word selection
+function storeWord(image){
+	let path = document.getElementById(image).src;
+	var word = path.slice(0, -4).split("/").pop();
+	words.push([image, word]);
+	console.log(image, word);
+}
+
+function checkMatch() {
+    if (words.length < 2) {
+        return;
+    }
+
+    const firstWord = words[0][1];
+    const secondWord = words[1][1];
+
+    if (combos[firstWord] && combos[firstWord].includes(secondWord)) {
+        changeCombined([firstWord, secondWord].sort().join(""));
+        changeImage(words[0][0]);
+        changeImage(words[1][0]);
+        words = [];
+        resetCellBg();
+        increaseScore();
+        if (score === clears[level]){
+            levelUp();
+        }
+    } else {
+        words = words.slice(-1);
+    }
+}
+
+function selectCell(cell_num) {
+    const cell = document.getElementById(`cell${cell_num}`);
+    if (cell) {
+        if (currentImage !== -1) {
+            resetCellBg();
+        }
+        cell.style.backgroundColor = "red";
+        currentImage = cell_num;
+    }
+    storeWord(`image${cell_num}`);
+    checkMatch();
+}
+
+// Image functions
+function changeImage(image){	
+	var newImage = images[Math.floor(Math.random() * images.length)];
+	document.getElementById(image).src=`./images/${newImage}.png`;
+	console.log(newImage);
+}
+
+function resetCellBg() {
+    const previousCell = document.getElementById(`cell${currentImage}`);
+    if (previousCell) {
+        previousCell.style.backgroundColor = "";
+    }
 }
 
 function changeCombined(image){
 	var d = document.getElementById("combined");
 	d.src=`./images/${image}.png`;
-	d.style.opacity = 1;
+	d.style.display = "block";
 	console.log(image);
 }
 
-function getWord(image,arr){
-	let path = document.querySelector(image).src;
-	var word = path.slice(0,-4).split("/").pop();
-	arr.push([image,word]);
-	console.log(image,word);
+function shuffleBoard() {
+    for (let i = 1; i < 17; i++) {
+        changeImage(`image${i}`);
+    }
+    resetCellBg();
 }
 
-function logCombo(arr){
-	console.log(arr[arr.length-2],arr[arr.length-1]);
+function resetBoard() {
+    for (let i = 1; i < 17; i++) {
+    	document.getElementById(`image${i}`).src=`./images/blank.png`;
+    }
+    resetCellBg();
 }
 
-function doSomething(image,arr,combos){
-	placeCursor(image);
-	getWord(image,arr);
-	logCombo(arr);
-	if (arr.length >= 2){
-		if (combos[arr[arr.length-2][1]].includes(arr[arr.length-1][1])){
-			var score = increaseScore();
-			console.log(score);
-			var level = Number(document.getElementById("level").innerHTML.slice(12));
-			console.log(level)
-			changeImage2(arr[arr.length-2][0]);
-			changeImage2(arr[arr.length-1][0]);
-			changeCombined(`${[arr[arr.length-2][1],arr[arr.length-1][1]].sort().join("")}`);
-			arr.length = 0;
-			if (score === clears[level]){
-				levelUp();
-			}
-		}
-	console.log(arr);
-	}
+// Timer functions
+function countdown() {
+    var seconds = 181;
+    function tick() {
+        if (!running) {
+            document.getElementById("timer").innerHTML = "時間 (Time): 3:00";
+            return;
+        }
+        var timer = document.getElementById("timer");
+        seconds--;
+        timer.innerHTML = `時間 (Time): ${Math.floor(seconds/60)}:${seconds%60 < 10 ? "0" : ""}${seconds%60}`;
+        if (seconds > -1) {
+            const counter = setTimeout(tick, 1000);
+        }
+        if (seconds === -1) {
+            document.getElementById("timer").innerHTML = "時間 (Time): 0:00";
+            endGame();
+        }
+    }
+    tick();
 }
 
-function setPos(id,left,top){
-	var d = document.getElementById(id);
-	d.style.position = "absolute";
-	d.style.left = left+'px';
-	d.style.top = top+'px';
+// Listener functions
+function addCellListeners() {
+    for (let i = 1; i < 17; i++) {
+        // Define the listener function and store it
+        const listener = () => selectCell(i);
+        cellListeners.set(i, listener);
+        document.getElementById(`cell${i}`).addEventListener("click", listener);
+    }
 }
 
-function setTimer() {
-	resetBoard()
-	document.getElementById("bgm").play()
-	document.getElementById("score").innerHTML = "分數 (Score): 0";
-	document.getElementById("start_grid").style.display = "none";
+function removeCellListeners() {
+    for (let i = 1; i < 17; i++) {
+        const cell = document.getElementById(`cell${i}`);
+        const listener = cellListeners.get(i);
+        if (listener) {
+            cell.removeEventListener("click", listener);
+        }
+    }
+}
+
+function addButtonListeners() {
+    document.getElementById("reset_button").addEventListener("click", shuffleBoard);
+    document.getElementById("end_button").addEventListener("click", endGame);
+}
+
+function removeButtonListeners() {
+    document.getElementById("reset_button").removeEventListener("click", shuffleBoard);
+    document.getElementById("end_button").removeEventListener("click", endGame);
+}
+
+function endGame() {
+    resetBoard();
+    resetCellBg();
+    removeCellListeners();
+    removeButtonListeners();
+    running = false;
+    updateScoreDisplay();
+    updateLevelDisplay();
+	document.getElementById("start_button").style.display = "block";
+	document.getElementById("reset_button").style.display = "none";
+	document.getElementById("end_button").style.display = "none";
+	document.getElementById("combined").style.display = "none";
+    alert("Game Over!\nYour final score is: " + score);
+    currentImage = -1;
+    score = 0;
+    level = 1;
+}
+
+function startGame() {
+    shuffleBoard();
+    addCellListeners();
+    addButtonListeners();
+    running = true;
 	document.getElementById("start_button").style.display = "none";
-	document.getElementById("cursor").style.display = "block";
-	document.getElementById("combined").style.opacity = 0;
+	document.getElementById("reset_button").style.display = "block";
+	document.getElementById("end_button").style.display = "block";
+	document.getElementById("bgm").play()
 	countdown();
 }
 
-function backToStart(){
-	document.getElementById("start_button").style.display = "block";
-	document.getElementById("start_grid").style.display = "block";
-	document.getElementById("cursor").style.display = "none";
-}
-
-function alertFunc() {
-  alert("Game Over!");
-}
-
-function countdown() {
-  var seconds = 181;
-  function tick() {
-    var timer = document.getElementById("timer");
-    seconds--;
-    timer.innerHTML = `時間 (Time): ${Math.floor(seconds/60)}:${seconds%60 < 10 ? "0" : ""}${seconds%60}`;
-    if (seconds > -1) {
-      const counter = setTimeout(tick, 1000);
+// Driver Code
+// Add event listeners to each image element
+document.getElementById("start_button").addEventListener("click", function() {
+        startGame();
     }
-    if (seconds === -1) {
-      document.getElementById("timer").innerHTML = "時間 (Time): 0:00";
-    	backToStart()
-      alertFunc()
-    }
-  }
-  tick();
-}
+);
 
-function resetBoard(){
-	for (let i = 1; i < 17; i++) {
-		changeImage(`image${i}`)
-	}
-}
-
-document.getElementById("bgm").addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-}, false);
-document.getElementById("bgm").loop = true
-
-resetBoard()
-for (let i = 1; i < 17; i++) {
-	document.getElementById(`image${i}`).addEventListener("click",function(){doSomething(`#image${i}`,combo,combos)});
-}
-
+var currentImage = -1;
+var score = 0;
+var level = 1;
+var running = false;
